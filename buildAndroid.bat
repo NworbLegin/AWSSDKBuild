7cc122e025ad76c9063896ed797f502a2306ba6d
Nigel on iSStiletto <nigel@impromptu-software.co.uk>
2021-10-20 23:26:00 +0100

    Latest Android build amendments
    - Only make release libraries
    - Copy the C libraries

--- buildAndroid.bat
+++ buildAndroid.bat
@@ -8,7 +8,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\T
 rem ** CREATE ANDROID DEBUG arm64-v8a **
 set abi=arm64-v8a
 set buildType=Debug
-call scripts/_buildAndroid.bat
+rem call scripts/_buildAndroid.bat
 
 rem ** CREATE ANDROID RELEASE arm64-v8a **
 set abi=arm64-v8a
@@ -19,7 +19,7 @@ call scripts/_buildAndroid.bat
 rem ** CREATE ANDROID DEBUG armeabi-v7a **
 set abi=armeabi-v7a
 set buildType=Debug
-call scripts/_buildAndroid.bat
+rem call scripts/_buildAndroid.bat
 
 
 rem ** CREATE ANDROID RELEASE armeabi-v7a **
--- scripts/_buildAndroid.bat
+++ scripts/_buildAndroid.bat
@@ -5,7 +5,7 @@ cd build
 mkdir android-%abi%-%buildType%
 cd android-%abi%-%buildType%
 
-cmake ../../aws-sdk-cpp/ -G "NMake Makefiles" -DTARGET_ARCH=ANDROID -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%buildType% -DBUILD_ONLY="kinesis;cognito-identity;cognito-sync;lambda;s3;apigateway;identity-management" -DBUILD_SHARED_LIBS=0 -DCMAKE_INSTALL_BINDIR=%outputPath%/%buildType%/%abi%/bin -DCMAKE_INSTALL_LIBDIR=%outputPath%/%buildType%/%abi%/lib -DCMAKE_INSTALL_INCLUDEDIR=%outputPath%/%buildType%/%abi%/include -DENABLE_TESTING=0 -DNDK_DIR=%androidNDKPath%
+cmake ../../aws-sdk-cpp/ -G "NMake Makefiles" -DTARGET_ARCH=ANDROID -DANDROID_ABI=%abi% -DCMAKE_BUILD_TYPE=%buildType% -DBUILD_ONLY="kinesis;cognito-identity;cognito-sync;lambda;s3;apigateway;identity-management" -DENABLE_TESTING=0 -DBUILD_SHARED_LIBS=0 -DCMAKE_INSTALL_BINDIR=%outputPath%/%buildType%/%abi%/bin -DCMAKE_INSTALL_LIBDIR=%outputPath%/%buildType%/%abi%/lib -DCMAKE_INSTALL_INCLUDEDIR=%outputPath%/%buildType%/%abi%/include -DENABLE_TESTING=0 -DNDK_DIR=%androidNDKPath%
 
 rem ** BUILD ANDROID DEBUG OUTPUT **
 cmake --build .
@@ -13,12 +13,13 @@ cmake --build . --target install
 
 rem ** Copy extern folder with curl, ssl etc
 set externalOutputPath=%outputpath:/=\%\%buildType%\%abi%\external
+echo externalOutputPath = %externalOutputPath%
 mkdir %externalOutputPath%
 xcopy /e /y /q external %externalOutputPath% 
 
-xcopy /e /t /q android-%abi%-%buildType%/.deps/install/lib/*.a %outputPath%/%buildType%/%abi%/lib
-
-rem ** Copy the c-libs as well
+xcopy /e /y /q .deps\install\lib\*.a %outputPath:/=\%\%buildType%\%abi%\lib
+mkdir %outputPath:/=\%/%buildType%/%abi%/install
+xcopy /e /y /q .deps\install\include %outputPath:/=\%\%buildType%\%abi%\include
 
 
 cd ..
